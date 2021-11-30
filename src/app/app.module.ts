da88c164cc4d6682065031e0fabb74cc7aaaeeaf
lrtwr <jeroen.leertouwer@outlook.com>
2021-11-30 08:16:23 +0100

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 @NgModule({
   declarations: [
+    MainMenuComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
